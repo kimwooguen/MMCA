0bae320f3ab470a08aba02be98661a69a6124fc1
mmca <cityman8@daum.net>
2018-04-19 16:39:05 +0900

    test

---
+++ app/src/main/java/kr/go/mmca/guide/common/TitleHandler.java
@@ -0,0 +1,155 @@
+package kr.go.mmca.guide.common;
+
+public class TitleHandler {
+	public static final String LANGUAGE_KOR_INDEX = "LANGUAGE01"; //한국어
+	public static final String LANGUAGE_ENG_INDEX = "LANGUAGE02"; //영어
+
+	public static final String LANGUAGE_KOR_TAG = "[국문]";
+	public static final String LANGUAGE_ENG_TAG = "[English]";
+
+	public static final String LANGUAGE_KOR_MUSEUM_TITLE = "국립현대미술관";
+	public static final String LANGUAGE_ENG_MUSEUM_TITLE = "MMCA";
+
+	public static final String LANGUAGE_KOR_EXHIBITHALL1_TITLE = "과천관";
+	public static final String LANGUAGE_ENG_EXHIBITHALL1_TITLE = "GWACHEON";
+
+	public static final String LANGUAGE_KOR_EXHIBITHALL2_TITLE = "서울관";
+	public static final String LANGUAGE_ENG_EXHIBITHALL2_TITLE = "SEOUL";
+
+	public static final String LANGUAGE_KOR_EXHIBITHALL3_TITLE = "덕수궁관";
+	public static final String LANGUAGE_ENG_EXHIBITHALL3_TITLE = "DEOKSUGUNG";
+
+	public static final String LANGUAGE_KOR_EXHIBITVIEW_TITLE = "전시안내";
+	public static final String LANGUAGE_ENG_EXHIBITVIEW_TITLE = "CURRENT EXHIBITION";
+
+	public static final String LANGUAGE_KOR_PRODUCTTYPE1_TITLE = "상설";
+	public static final String LANGUAGE_ENG_PRODUCTTYPE1_TITLE = "PERMANENT EXHIBITION";
+
+	public static final String LANGUAGE_KOR_PRODUCTTYPE2_TITLE = "기획";
+	public static final String LANGUAGE_ENG_PRODUCTTYPE2_TITLE = "SPECIAL EXHIBITION";
+
+	public static final String LANGUAGE_KOR_PRODUCTPAY1_TITLE = "유료";
+	public static final String LANGUAGE_ENG_PRODUCTPAY1_TITLE = "CHARGED";
+
+	public static final String LANGUAGE_KOR_PRODUCTPAY2_TITLE = "무료";
+	public static final String LANGUAGE_ENG_PRODUCTPAY2_TITLE = "FREE";
+
+	public static final String LANGUAGE_KOR_LIKE_TITLE = "좋아요";
+	public static final String LANGUAGE_ENG_LIKE_TITLE = "LIKE";
+
+	public static final String LANGUAGE_KOR_SHARE_TITLE = "공유하기";
+	public static final String LANGUAGE_ENG_SHARE_TITLE = "SHARE";
+
+	public static final String LANGUAGE_KOR_PRODUCT_DESC_TITLE = "전시소개";
+	public static final String LANGUAGE_ENG_PRODUCT_DESC_TITLE = "CURRENT EXHIBITION";
+
+	public static final String LANGUAGE_KOR_MAIN_BTN1_TITLE = "작품안내";
+	public static final String LANGUAGE_ENG_MAIN_BTN1_TITLE = "ARTWORKS";
+
+	public static final String LANGUAGE_KOR_MAIN_BTN2_TITLE = "가이드 투어";
+	public static final String LANGUAGE_ENG_MAIN_BTN2_TITLE = "TOURS";
+
+	public static final String LANGUAGE_KOR_MAIN_BTN3_TITLE = "메뉴";
+	public static final String LANGUAGE_ENG_MAIN_BTN3_TITLE = "MENU";
+
+	public static final String LANGUAGE_KOR_CONFIG_TITLE = "관 바꾸기";
+	public static final String LANGUAGE_ENG_CONFIG_TITLE = "SELECT A BRANCH";
+
+	public static final String LANGUAGE_KOR_CONFIG_BTN1_TITLE = "현재 전시";
+	public static final String LANGUAGE_ENG_CONFIG_BTN1_TITLE = "CURRENT EXHIBITIONS";
+
+	public static final String LANGUAGE_KOR_CONFIG_BTN2_TITLE = "작품검색";
+	public static final String LANGUAGE_ENG_CONFIG_BTN2_TITLE = "TYPE TO SEARCH";
+
+	public static final String LANGUAGE_KOR_CONFIG_BTN3_TITLE = "길찾기";
+	public static final String LANGUAGE_ENG_CONFIG_BTN3_TITLE = "MAP";
+
+	public static final String LANGUAGE_KOR_CONFIG_BTN4_TITLE = "관람안내";
+	public static final String LANGUAGE_ENG_CONFIG_BTN4_TITLE = "VISIT";
+
+	public static final String LANGUAGE_KOR_CONFIG_BTN5_TITLE = "편의시설";
+	public static final String LANGUAGE_ENG_CONFIG_BTN5_TITLE = "CONVENIENT FACILITIES";
+
+	public static final String LANGUAGE_KOR_CONFIG_BTN6_TITLE = "마이페이지";
+	public static final String LANGUAGE_ENG_CONFIG_BTN6_TITLE = "MY PAGE";
+
+	public static final String LANGUAGE_KOR_CONFIG_BTN7_TITLE = "공지사항";
+	public static final String LANGUAGE_ENG_CONFIG_BTN7_TITLE = "NOTICE";
+
+	public static final String LANGUAGE_KOR_CONFIG_BTN8_TITLE = "앱사용가이드";
+	public static final String LANGUAGE_ENG_CONFIG_BTN8_TITLE = "APP USER GUIDE";
+
+	public static final String LANGUAGE_KOR_CONFIG_BTN9_TITLE = "설정";
+	public static final String LANGUAGE_ENG_CONFIG_BTN9_TITLE = "SETTING";
+
+	public static final String LANGUAGE_KOR_CONFIG_BTN10_TITLE = "홈";
+	public static final String LANGUAGE_ENG_CONFIG_BTN10_TITLE = "HOME";
+
+
+	public static final String EXHIBIT_SEARCH_CHK_MESSAGE_KOR_TEXT = "작품명 또는 작품번호를 입력해 주세요.";
+	public static final String EXHIBIT_SEARCH_CHK_MESSAGE_ENG_TEXT = "ENTER ARTWORK NUMBER";
+
+	public static final String MYPAGE_TAB_MENU1_KOR_TEXT = "좋아요";
+	public static final String MYPAGE_TAB_MENU1_ENG_TEXT = "LIKE";
+
+	public static final String MYPAGE_TAB_MENU3_KOR_TEXT = "스템프";
+	public static final String MYPAGE_TAB_MENU3_ENG_TEXT = "STAMP";
+
+	public static final String MYPAGE_TAB_SUB_MENU1_KOR_TEXT = "전시";
+	public static final String MYPAGE_TAB_SUB_MENU1_ENG_TEXT = "EXHIBITION";
+
+	public static final String MYPAGE_TAB_SUB_MENU2_KOR_TEXT = "작품";
+	public static final String MYPAGE_TAB_SUB_MENU2_ENG_TEXT = "ARTWORKS";
+
+	//검색하기
+	public static final String FIND_ARTICLE_NO_KOR_TEXT = "작품번호 : ";
+	public static final String FIND_ARTICLE_NO_ENG_TEXT = "ARTWORK NUMBER : ";
+
+	public static final String FIND_ARTICLE_NAME_KOR_TEXT = "작품 : ";
+	public static final String FIND_ARTICLE_NAME_ENG_TEXT = "ARTWORKS : ";
+
+	public static final String FIND_ARTICLE_ARTIST_KOR_TEXT = "작가 : ";
+	public static final String FIND_ARTICLE_ARTIST_ENG_TEXT = "ARTISTS : ";
+
+	public static final String GUIDE_TOUR_START_KOR_TEXT = "가이드 투어 시작하기";
+	public static final String GUIDE_TOUR_START_ENG_TEXT = "BEGIN A GUIDED TOUR";
+
+	public static final String EXHIBIT_FIND_KOR_MSG1_TEXT = "주변에 근접한 작품이 없습니다";
+	public static final String EXHIBIT_FIND_ENG_MSG1_TEXT = "작품안내 운영 전시 목록 보기";
+
+	public static final String EXHIBIT_FIND_KOR_MSG2_TEXT = "THERE IS NO ARTWORK NEARBY";
+	public static final String EXHIBIT_FIND_ENG_MSG2_TEXT = "SEE THE CURRENT EXHIBITIONS";
+
+	public static final String SELECT_DEST_KOR_TEXT = "목적지 선택";
+	public static final String SELECT_DEST_ENG_TEXT = "DESTINATION";
+
+	public static final String LANGUAGE_KOR_AUDIO_GUIDE_ON_TITLE = "작품안내 운영";
+	public static final String LANGUAGE_ENG_AUDIO_GUIDE_ON_TITLE = "GUIDE IN OPERATION";
+
+	public static final String LANGUAGE_KOR_AUDIO_GUIDE_OFF_TITLE = "작품안내 없음";
+	public static final String LANGUAGE_ENG_AUDIO_GUIDE_OFF_TITLE = "NO INTRODUCTION OF ARTWORKS";
+
+	public static final String LANGUAGE_KOR_EXHIBIT_NAVI_TITLE = "전시실 찾아가기";
+	public static final String LANGUAGE_ENG_EXHIBIT_NAVI_TITLE = "GO TO THE GALLERY";
+
+	public static final String LANGUAGE_KOR_VERSION_TITLE = "버전";
+	public static final String LANGUAGE_ENG_VERSION_TITLE = "VERSION";
+
+	public static final String LANGUAGE_KOR_LANG_TITLE = "언어설정";
+	public static final String LANGUAGE_ENG_LANG_TITLE = "SELECT LANGUAGE";
+
+	public static final String LANGUAGE_KOR_BLE_TITLE = "블루투스";
+	public static final String LANGUAGE_ENG_BLE_TITLE = "BLUETOOTH";
+
+	public static final String LANGUAGE_KOR_EXHIBIT_ROOM_TITLE = "전시실";
+	public static final String LANGUAGE_ENG_EXHIBIT_ROOM_TITLE = "EXHIBITION ROOM";
+
+	public static final String LANGUAGE_KOR_EXHIBIT_OUT_TITLE = "미술관 내에서만 사용할 수 있습니다";
+	public static final String LANGUAGE_ENG_EXHIBIT_OUT_TITLE = "IT ONLY WORKS INSIDE THE MUSEUM";
+
+	public static final String LANGUAGE_KOR_PRODUCT_PREPARING_TITLE = "새로운 전시를 준비 중입니다";
+	public static final String LANGUAGE_ENG_PRODUCT_PREPARING_TITLE = "PREPARING A NEW EXHIBITION";
+
+	public static final String LANGUAGE_KOR_GUIDETOUR_PREPARING_TITLE = "새로운 가이드투어를 준비 중입니다";
+	public static final String LANGUAGE_ENG_GUIDETOUR_PREPARING_TITLE = "PREPARING A NEW TOUR";
+}
